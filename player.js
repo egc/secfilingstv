b322d218780b5f358f4cd1b5c3b01b993719cd3b
Justin Kuepper <justin.kuepper@gmail.com>
2013-09-20 11:08:24 -0600

    added fancybox modal popups

--- player.js
+++ player.js
@@ -12,7 +12,7 @@ $(function() {
 			var description = item['content']['$t'];
 			var views = item['yt$statistics']['viewCount'];
 			
-			htmlStringFeature += '<div class="three-fourth"><div style="width:700px; min-height:300px;"><a href="'+link+'"><img src="'+thumbnail+'" style="width:700px; height:300px;"></a></div></div>';
+			htmlStringFeature += '<div class="three-fourth"><div style="width:700px; min-height:300px;"><a class="fancybox-media" href="'+link+'"><img src="'+thumbnail+'" style="width:700px; height:300px;"></a></div></div>';
 			htmlStringFeature += '<div class="one-fourth last"><h4>'+title+'</h4><ul class="post-meta"><li><i class="icon-user"></i>'+views+' Views</li></ul><p>'+description.substr(0,200)+'...</p></div>';
 		
 		});
@@ -30,11 +30,19 @@ $(function() {
 			
 			if (i == 1) { last = 'last'; };
 			
-			htmlStringExecutive += '<div class="blog-post layout-3 '+last+'"><div class="media-holder"></div><span class="media-holder"><div style="width:340px; min-height:200px;"><a href="'+link+'"><img src="'+thumbnail+'" style="width:340px; height:200px;"></a></div></span>';
-			htmlStringExecutive += '<div class="permalink"><h4><a href="'+link+'">'+title+'</a></h4></div>';
+			htmlStringExecutive += '<div class="blog-post layout-3 '+last+'"><div class="media-holder"></div><span class="media-holder"><div style="width:340px; min-height:200px;"><a class="fancybox-media" href="'+link+'"><img src="'+thumbnail+'" style="width:340px; height:200px;"></a></div></span>';
+			htmlStringExecutive += '<div class="permalink"><h4><a class="fancybox-media" href="'+link+'">'+title+'</a></h4></div>';
 			htmlStringExecutive += '<ul class="post-meta"><li><i class="icon-user"></i>'+views+' Views</li></ul>';
 			htmlStringExecutive += '<div class="post-intro"><p>'+description.substr(0,200)+'...</p></div></div>';
 		});
 		$('#executive-videos').html(htmlStringExecutive);
+	});
+	
+	$('.fancybox-media').fancybox({
+		openEffect: 'none',
+		closeEffect: 'none',
+		helpers : {
+			media: {}
+		}
 	});
 });
